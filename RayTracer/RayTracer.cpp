936e40ccd3e7ff6eb9de7215c0fff0eb35789e5e
tomboyanushka <anushkanair20@gmail.com>
2021-06-19 17:49:48 -0400

    cleanup + progress indicator

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -1,8 +1,15 @@
 ///Ray Tracing in One Weekend
 ///Peter Shirley
-
-
+#include <Windows.h>
 #include <iostream>
+#include <sstream>
+
+#define DBOUT( s )            \
+{                             \
+   std::wostringstream os_;    \
+   os_ << s;                   \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
 
 int main() {
 
@@ -13,8 +20,11 @@ int main() {
     // Render the image
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
-        for (int i = 0; i < image_width; ++i) {
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        DBOUT("\rScanlines remaining: " << j);
+        for (int i = 0; i < image_width; ++i)
+        {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.25;
@@ -26,4 +36,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    DBOUT("\nDone.\n");
 }
