db02bcfd114fde0c5a1badc5b7d25effee8b08b0
tomboyanushka <anushkanair20@gmail.com>
2021-06-30 19:10:29 -0400

    Adding a sphere

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -14,8 +14,22 @@
    OutputDebugStringW( os_.str().c_str() );  \
 }
 
+bool HitSphere(const Point3& center, double radius, const Ray& ray)
+{
+    Vec3 oc = ray.Origin() - center;
+    auto a = Dot(ray.Direction(), ray.Direction());
+    auto b = 2.0 * Dot(oc, ray.Direction());
+    auto c = Dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4*a*c;
+    return (discriminant > 0);
+}
+
 Color RayColor(const Ray& r)
 {
+    if (HitSphere(Vec3(0, 0, -1), 0.5f, r))
+    {
+        return Vec3(1, 0, 0);
+    }
     Vec3 unitDirection = UnitVector(r.Direction());
     auto t = 0.5f * (unitDirection.y() + 1.0f);
     return (1.0f - t) * Color(1.0f, 1.0f, 1.0f) + t * Color(0.5f, 0.7f, 1.0f);
